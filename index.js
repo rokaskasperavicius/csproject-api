7684a1feadf523d2283446d4815ae8d5457d25ea
Rokas Kasperavicius <goodname258@gmail.com>
2022-02-27 11:07:48 +0100

    test

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'CS Project API description',
+    version: '1.0.0',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+  },
+}
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./*.js'],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ */
 app.get('/', (req, res) => {
   res.send('Hello World!')
   console.log('User: ' + req.ip)
