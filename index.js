8394cdaf3da09d5ef306e45c60947d30f22c697f
Rokas Kasperavicius <goodname258@gmail.com>
2022-02-27 20:31:10 +0100

    SwaggerHub docs

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const { Pool } = require('pg')
+const port = process.env.PORT || 5000
+
+const cors = require('cors')
 
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
+
+require('dotenv').config()
+
+app.use(express.json())
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
 
 const swaggerDefinition = {
   openapi: '3.0.0',
@@ -25,18 +39,94 @@ const options = {
 
 const swaggerSpec = swaggerJSDoc(options)
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    sslmode: 'require',
+    rejectUnauthorized: false,
+  },
+})
 
 /**
  * @swagger
- * /users:
- *   get:
- *     summary: Retrieve a list of JSONPlaceholder users
- *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *  /product:
+ *    post:
+ *      tags: [Products]
+ *      summary: Add a new product
+ *      description: Add a new product with a given category and sub-category.
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+ *                categoryName:
+ *                  type: string
+ *                  description: Category name
+ *                  example: food
+ *                subCategoryName:
+ *                  type: string
+ *                  description: Sub-category name
+ *                  example: vegetables
+ *                name:
+ *                  type: string
+ *                  description: Name of the product
+ *                  example: carrots
+ *                note:
+ *                  type: string
+ *                  description: User specific custom note
+ *                  example: These carrots are really good
+ *      responses:
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    example: true
+ *        500:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  success:
+ *                    type: boolean
+ *                    example: false
  */
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log('User: ' + req.ip)
+app.post('/product', async (req, res) => {
+  const { categoryName, subCategoryName, name, note } = req.body
+
+  const client = await pool.connect()
+
+  client.query(
+    `
+    INSERT INTO PRODUCTS(sub_category_id, name, note)
+      VALUES(
+      (SELECT id
+        FROM sub_categories
+        WHERE name = '${subCategoryName.toLowerCase()}' AND category_id = (SELECT id FROM categories WHERE name = '${categoryName.toLowerCase()}')
+      ),
+      '${name}',
+      '${note}'
+    );
+  `,
+    (err) => {
+      client.release()
+
+      if (err) {
+        console.log(err)
+        res.json({ success: false })
+      } else {
+        res.json({ success: true })
+      }
+    }
+  )
 })
 
 setInterval(() => {
@@ -46,3 +136,21 @@ setInterval(() => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// app.get('/test', async (req, res) => {
+//   const client = await pool.connect()
+
+//   client.query('SELECT name FROM categories', (err, result) => {
+//     client.release()
+
+//     if (err) {
+//       console.log(err.code)
+//       res.json({ success: false })
+//     } else {
+//       res.json({
+//         success: true,
+//         data: result.rows.map(({ name }) => name),
+//       })
+//     }
+//   })
+// })
